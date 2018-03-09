37b7f792a05ff9c535be4b385fe776d5a03c5c2b
Vladimir <petlyuk.vova@gmail.com>
2018-03-09 23:10:32 +0200

    HW1 final

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
-private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+    private static final Logger log = getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("debug users");
+        log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
